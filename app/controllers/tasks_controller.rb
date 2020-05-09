4001a4f1c505547a0d4aa84d6a45d655d59d6136
eriko.fujita <tangtin.waileizi@gmail.com>
2020-05-09 01:40:36 +0000

    task_controller def create modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,14 +16,14 @@ class TasksController < ApplicationController
   
   
   def create
-    @task = Task.new(taks_params)
+    @task = Task.new(task_params)
     
     if @task.save
-      flash[:success] = 'タスクが正常に投稿されました'
+      flash[:success] = 'Taskが正常に投稿されました'
       redirect_to @task
       
     else
-      flash.now[:danger] = 'タスクが投稿されませんでした'
+      flash.now[:danger] = 'Taskが投稿されませんでした'
       render :new
     end
   end
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   
   
   
-  privaate
+  private
   
   # Strong Parameter
   def task_params
