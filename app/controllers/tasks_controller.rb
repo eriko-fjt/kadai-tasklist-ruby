5a2960c4744f925640da123363426fd627ccb283
eriko.fujita <tangtin.waileizi@gmail.com>
2020-05-11 13:53:24 +0000

    change pagenation, add db-seed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+    @tasks = Task.order(id: :desc).page(params[:page]).per(5)
   end
   
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# バリデーションでstatusの文字数を制限しているので、注意
+
+(1..100).each do |number|
+  Task.create!(content: 'test content ' + number.to_s, status: 'status ' + number.to_s)
+end
