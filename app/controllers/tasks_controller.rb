0c74a662d91e3e2f9163f87e56d195975b4a83ce
eriko.fujita <tangtin.waileizi@gmail.com>
2020-05-18 10:09:21 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,17 @@
 class TasksController < ApplicationController
   
-  before_action :require_user_logged_in, only: [:index, :show, :edit]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+    if logged_in?
+    #  @task = current_user.tasks.build # form_with用課題では、フォーム無いので使わず
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]) #一覧表示用
+    end
+    
+    # 元々@tasks = Task.order(id: :desc).page(params[:page]).per(5)
+     
   end
   
   
@@ -19,13 +26,14 @@ class TasksController < ApplicationController
   
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
       
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした'
       render :new
     end
@@ -56,7 +64,8 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to tasks_url 
+    # 今回の課題にはNG redirect_back(fallback_location: root_path)
   end
   
   
@@ -64,7 +73,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
   
   # Strong Parameter
@@ -73,6 +82,12 @@ class TasksController < ApplicationController
   end
   
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
   
   
 end
